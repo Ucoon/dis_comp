08b5fa5b1cd464c25194e7a3721a44fc09afaccf
张雅彬 <981213@gerrit.sdp.nd>
2019-04-08 23:05:55 +0800

    提交参数配置

--- BusinessComp/search_comp/src/main/java/com/bristua/ft/component/search/business/SearchBuisinessImpl.java
+++ BusinessComp/search_comp/src/main/java/com/bristua/ft/component/search/business/SearchBuisinessImpl.java
@@ -2,6 +2,10 @@ package com.bristua.ft.component.search.business;
 import android.support.annotation.NonNull;
 import com.bristua.framework.define.IFlutterResult;
 import com.bristua.framework.system.IBusinessManager;
+import com.bristua.ft.component.search.domain.HistorySearchDomain;
+import com.bristua.ft.component.search.domain.ISearchDomain;
+import com.bristua.ft.component.search.domain.SearchFactory;
+import com.bristua.ft.component.search.repository.SearchProduct;
 import com.bristua.ft.component.search.repository.SearchRepository;
 
 /**
@@ -13,17 +17,29 @@ public class SearchBuisinessImpl implements ISearchBusiness, IBusinessManager {
     @Override
     public void exec(@NonNull String pProtocol, @NonNull IFlutterResult pCallback) {
 
+        //此处进入解析操作方式，1.解析json报文，读取type类型
 
     }
 
     @Override
     public void findHistoryFromKey(@NonNull SearchRepository pRepository, @NonNull IFlutterResult pCallback) {
 
+        ISearchDomain domain= SearchFactory.getFactory().getDommain(pRepository.getType());
+        if(domain ==  null){
+            return;
+        }
 
+        //domain执行操作
+        domain.findData(pCallback);
     }
 
     @Override
     public void findProductsFromName(@NonNull SearchRepository pRepository, @NonNull IFlutterResult pCallback) {
-
+        ISearchDomain domain= SearchFactory.getFactory().getDommain(pRepository.getType());
+        if(domain ==  null){
+            return;
+        }
+        //domain执行操作
+        domain.findData(pCallback);
     }
 }
--- BusinessComp/search_comp/src/main/java/com/bristua/ft/component/search/domain/ISearchDomain.java
+++ BusinessComp/search_comp/src/main/java/com/bristua/ft/component/search/domain/ISearchDomain.java
@@ -1,7 +1,17 @@
 package com.bristua.ft.component.search.domain;
 
+import android.support.annotation.NonNull;
+
+import com.bristua.framework.define.IFlutterResult;
+
 /**
+ * 获取数据源
  * @author richsjeson
  */
 public interface ISearchDomain {
+    /**
+     * 执行数据源操作
+     * @param pResult  向flutter层上层进行回调操作
+     */
+    void findData(@NonNull IFlutterResult pResult);
 }
