80456242f38886d2d3b7761d8ad062166246abf6
张雅彬 <981213@gerrit.sdp.nd>
2019-04-21 15:34:39 +0800

    提交备份

---
+++ BusinessComp/order/src/main/java/com/ft/bristua/component/order/OrderComponent.java
@@ -0,0 +1,16 @@
+package com.ft.bristua.component.order;
+
+import com.bristua.framework.define.IComponent;
+import com.nd.adhoc.framework.BaseComponent;
+import com.nd.sdp.android.serviceloader.annotation.Service;
+
+/**
+ * 订单模组组件 包含订单详情和提交订单
+ * @author richsjeson
+ */
+@Service(IComponent.class)
+public class OrderComponent extends BaseComponent implements IComponent {
+
+
+
+}
