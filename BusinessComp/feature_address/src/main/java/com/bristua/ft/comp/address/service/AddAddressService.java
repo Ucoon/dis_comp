cb53ee7b068637e3a11034a31f55aaa590d669cc
RushKing <1054917071@qq.com>
2019-04-22 18:35:56 +0800

    地址获取data为空问题

--- BusinessComp/feature_address/src/main/java/com/bristua/ft/comp/address/service/AddAddressService.java
+++ BusinessComp/feature_address/src/main/java/com/bristua/ft/comp/address/service/AddAddressService.java
@@ -2,6 +2,7 @@ package com.bristua.ft.comp.address.service;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.text.TextUtils;
 
 import com.alibaba.fastjson.JSON;
 import com.bristua.framework.appconfig.AppConfig;
@@ -29,38 +30,40 @@ public class AddAddressService {
 
     /**
      * 此处加入rxjava相关的代码
+     *
      * @param pAddrsss
      * @param pResult
      */
-    public static void add(@NonNull  AddressWrapper pAddrsss, @NonNull final IFlutterResult pResult){
+    public static void add(@NonNull AddressWrapper pAddrsss, @NonNull final IFlutterResult pResult) {
         AndroidRxManager.clear();
-        AppContext appContext= AppConfig.getInstance().getAppContext();
-        Context context=appContext.getContext();
+        AppContext appContext = AppConfig.getInstance().getAppContext();
+        Context context = appContext.getContext();
         //执行retrofit 下的rx模式
-        Retrofit retrofit= HttpsModule.getInstance().getRetrofit();
-        if(retrofit == null){
+        Retrofit retrofit = HttpsModule.getInstance().getRetrofit();
+        if (retrofit == null) {
             String errorTip = ProtocolFactory.convertToJson(context.getResources().getString(R.string.address_error_http), 500, null);
-            pResult.success(errorTip,500,null);
+            pResult.success(errorTip, 500, null);
             return;
         }
-        IAddressApi restApi= retrofit.create(IAddressApi.class);;
+        IAddressApi restApi = retrofit.create(IAddressApi.class);
+        ;
         RequestBody body = RequestBody.create(okhttp3.MediaType.parse("application/json;charset=utf-8"), JSON.toJSON(pAddrsss).toString());
-        Disposable disposable=restApi.add(body)
+        Disposable disposable = restApi.add(body)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Consumer<String>() {
                     @Override
                     public void accept(String result) {
                         AndroidRxManager.clear();
-                        pResult.success(result,200,null);
+                        pResult.success(!TextUtils.isEmpty(result) ? result : "{\"code\":200,\"msg\":\"保存成功\",\"data\":\"\"}", 200, null);
 
                     }
                 }, new Consumer<Throwable>() {
                     @Override
-                    public void accept(Throwable throwable){
+                    public void accept(Throwable throwable) {
                         AndroidRxManager.clear();
                         String errorTip = ProtocolFactory.convertToJson(throwable.getLocalizedMessage(), 500, null);
-                        pResult.success(errorTip,500,null);
+                        pResult.success(errorTip, 500, null);
 
                     }
                 });
