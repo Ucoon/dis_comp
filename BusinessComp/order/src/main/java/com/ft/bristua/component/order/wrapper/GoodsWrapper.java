d615af0f25f1a8c7e6649d07818f9f8ea381c2f5
ym_windows <18720625976@163.com>
2019-04-30 23:54:47 +0800

    publish order  1.0.8

--- BusinessComp/order/src/main/java/com/ft/bristua/component/order/wrapper/GoodsWrapper.java
+++ BusinessComp/order/src/main/java/com/ft/bristua/component/order/wrapper/GoodsWrapper.java
@@ -3,8 +3,25 @@ package com.ft.bristua.component.order.wrapper;
 public class GoodsWrapper {
 
     private String goodsId;
-
     private int buyNum;
+    private String productId;
+    private String specMsg;
+
+    public String getSpecMsg() {
+        return specMsg;
+    }
+
+    public void setSpecMsg(String specMsg) {
+        this.specMsg = specMsg;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
 
     public String getGoodsId() {
         return goodsId;
