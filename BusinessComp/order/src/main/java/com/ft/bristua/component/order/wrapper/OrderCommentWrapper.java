08e9c37e4db578f4c25ebd18a743d746307de824
ym_windows <18720625976@163.com>
2019-05-04 20:17:00 +0800

    publish order confirm 1.1.9

--- BusinessComp/order/src/main/java/com/ft/bristua/component/order/wrapper/OrderCommentWrapper.java
+++ BusinessComp/order/src/main/java/com/ft/bristua/component/order/wrapper/OrderCommentWrapper.java
@@ -10,6 +10,22 @@ public class OrderCommentWrapper {
 
     private List<GoodsEvaluateEntity> goods;
 
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public List<GoodsEvaluateEntity> getGoods() {
+        return goods;
+    }
+
+    public void setGoods(List<GoodsEvaluateEntity> goods) {
+        this.goods = goods;
+    }
+
     public OrderCommentWrapper(String orderId, List<GoodsEvaluateEntity> goods) {
         this.orderId = orderId;
         this.goods = goods;
